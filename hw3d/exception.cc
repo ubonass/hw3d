4d2d5c5d36b491202bb22d11905a2409f6ad58d0
jeffrey.liu <jeffrey.liu@cloudsky.com>
2025-12-14 21:10:44 +0800

    Base Exception support

---
+++ hw3d/exception.cc
@@ -0,0 +1,35 @@
+﻿#include "exception.h"
+
+#include <sstream>
+
+namespace hw3d {
+
+Exception::Exception(int line, const char* file) noexcept
+    : line_(line), file_(file) {}
+
+const char* Exception::what() const noexcept {
+  std::ostringstream oss;
+  oss << GetType() << std::endl << GetOriginString();
+  what_buffer_ = oss.str();
+  return what_buffer_.c_str();
+}
+
+const char* Exception::GetType() const noexcept {
+  return "hw3d Exception";
+}
+
+int Exception::GetLine() const noexcept {
+  return line_;
+}
+
+const std::string& Exception::GetFile() const noexcept {
+  return file_;
+}
+
+std::string Exception::GetOriginString() const noexcept {
+  std::ostringstream oss;
+  oss << "[File] " << file_ << std::endl << "[Line] " << line_;
+  return oss.str();
+}
+
+}  // namespace hw3d
---
+++ hw3d/exception.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <exception>
+#include <string>
+
+namespace hw3d {
+
+class Exception : public std::exception {
+ public:
+  Exception(int line, const char* file) noexcept;
+
+  virtual const char* what() const noexcept override;
+
+  virtual const char* GetType() const noexcept;
+
+  int GetLine() const noexcept;
+
+  const std::string& GetFile() const noexcept;
+
+  std::string GetOriginString() const noexcept;
+
+ protected:
+  mutable  std::string what_buffer_;
+
+ private:
+  int line_;
+  std::string file_;
+};
+
+}  // namespace hw3d
