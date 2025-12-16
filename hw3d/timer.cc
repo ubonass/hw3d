593e7fc8ca60fb9b54e5267645ce2886a5105d43
jeffrey.liu <jeffrey.liu@cloudsky.com>
2025-12-16 11:45:25 +0800

    Add base add timer

---
+++ hw3d/timer.cc
@@ -0,0 +1,21 @@
+﻿#include "Timer.h"
+
+namespace hw3d {
+
+Timer::Timer() {
+  last_ = std::chrono::steady_clock::now();
+}
+
+float Timer::Mark() {
+  const auto old = last_;
+  last_ = std::chrono::steady_clock::now();
+  const std::chrono::duration<float> frameTime = last_ - old;
+  return frameTime.count();
+}
+
+float Timer::Peek() const {
+  return std::chrono::duration<float>(std::chrono::steady_clock::now() - last_)
+      .count();
+}
+
+}  // namespace hw3d
---
+++ hw3d/timer.h
@@ -0,0 +1,26 @@
+﻿
+#pragma once
+#include <chrono>
+
+namespace hw3d {
+// Timer class for measuring elapsed time intervals.
+class Timer {
+ public:
+  // Constructor. Initializes the timer and sets the starting point.
+  Timer();
+
+  // Returns the time elapsed (in seconds) since the last call to Mark().
+  // Also resets the starting point to now.
+  float Mark();
+
+  // Returns the time elapsed (in seconds) since the last call to Mark() or
+  // since construction, but does not reset the starting point.
+  float Peek() const;
+
+ private:
+  // Stores the last time point when Mark() was called or when the timer was
+  // constructed.
+  std::chrono::steady_clock::time_point last_;
+};
+
+}  // namespace hw3d
